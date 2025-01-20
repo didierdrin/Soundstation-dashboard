3dc7f04691bc33a60c0b862d9a47d0b7bf32ea65
Didier Nsengiyumva <nsedidier@gmail.com>
2025-01-20 17:51:55 +0200

    latest updates

--- firebaseApp.js
+++ firebaseApp.js
@@ -3,6 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage"; 
 
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -19,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
-export const storage = app.storage();
+export const storage = getStorage(app);
